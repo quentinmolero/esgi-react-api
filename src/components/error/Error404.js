9469b9b66933310db1afae362c9f0ede5a7f6780
Quentin MOLERO <quentin.molero@estiam.com>
2021-01-15 08:24:28 +0100

    fix 404 error page

--- src/components/error/Error404.js
+++ src/components/error/Error404.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import "../../styles/Error.css";
 
 const Error404 = () => {
     return (
         <div>
-            404 - Not found
+            <p className="error-code">404</p>
+            <p className="error-text">Not found</p>
+            <button className="error-button" onClick={() => window.location.pathname = '/'}>Take me to the Home page</button>
         </div>
     );
 }
